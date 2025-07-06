0c04add688f94e9a8b2f2bd8b605107d2b77a779
Md Hasin Arafat Al Sifat <98542863+Arafat1969@users.noreply.github.com>
2025-07-07 03:20:51 +0600

    Trying to deploy

--- FrontEnd/src/services/apiConfig.js
+++ FrontEnd/src/services/apiConfig.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
-// const API_BASE_URLs = {
-//   USER_SERVICE: 'http://localhost:8085/api/v1/user-service',
-//   DONATION_SERVICE: 'http://localhost:8080/api/v1/donation-service',
-//   EVENT_SERVICE: 'http://localhost:8090/api/v1/event-service'
-// };
-
 const API_BASE_URLs = {
-  USER_SERVICE: 'http://74.225.246.127:8085/api/v1/user-service',
-  DONATION_SERVICE: 'http://74.225.246.127:8080/api/v1/donation-service',
-  EVENT_SERVICE: 'http://74.225.246.127:8090/api/v1/event-service'
+  USER_SERVICE: 'http://localhost:8085/api/v1/user-service',
+  DONATION_SERVICE: 'http://localhost:8080/api/v1/donation-service',
+  EVENT_SERVICE: 'http://localhost:8090/api/v1/event-service'
 };
+
+// const API_BASE_URLs = {
+//   USER_SERVICE: 'http://74.225.246.127:8085/api/v1/user-service',
+//   DONATION_SERVICE: 'http://74.225.246.127:8080/api/v1/donation-service',
+//   EVENT_SERVICE: 'http://74.225.246.127:8090/api/v1/event-service'
+// };
 
 const createAuthenticatedAxios = () => {
   const instance = axios.create();
